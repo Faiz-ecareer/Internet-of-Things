1b00eba0e801489da3cfcbfd04a97cb044a17ccb
FAIZ AHMAD <52060075+Faiz-ecareer@users.noreply.github.com>
2020-02-29 01:05:47 +0530

    auto connect with blynk

---
+++ nodemcu_AutoConnect_with_blynk.ino
@@ -0,0 +1,48 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <BlynkSimpleEsp8266.h>
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+char auth [] = "d7ac5ca2768b4c6593f204cb408efd74";
+ 
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("Power","password");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+   
+    
+    Blynk.begin(auth,WiFi.SSID().c_str(),WiFi.psk().c_str());
+
+    //Serial.println(WiFi.SSID().c_str());
+    //Serial.println(WiFi.psk().c_str());
+    
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    
+}
