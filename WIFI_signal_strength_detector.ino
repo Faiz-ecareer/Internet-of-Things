c8449eba79ee3da09ac7b72f859d3a5dd2ba4df7
FAIZ AHMAD <52060075+Faiz-ecareer@users.noreply.github.com>
2020-02-29 01:08:50 +0530

    WIFI_signal_strength_detector

---
+++ WIFI_signal_strength_detector.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+const char* SSID = "Hotshot";
+#define relay1 16
+
+int32_t getRSSI(const char* target_ssid)
+{
+  byte available_networks = WiFi.scanNetworks();
+  for(int network = 0; network < available_networks; network++)
+  {
+    if(WiFi.SSID(network).compareTo(target_ssid) == 0)
+    {
+      return WiFi.RSSI(network);
+    }
+  }
+  return 0;
+}
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(relay1, OUTPUT);
+}
+void loop()
+{
+  int32_t rssi = getRSSI(SSID);
+  Serial.print("Signal strength: ");
+  Serial.print(rssi);
+  Serial.print("dBm");
+
+ if (rssi > (-70) && rssi != 0)
+ {
+  digitalWrite(relay1, LOW);
+  Serial.println("ON");
+ }
+ else
+ {
+  digitalWrite(relay1, HIGH);
+  Serial.println("OFF");
+ }
+}
+
